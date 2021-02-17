76c12260f36ea1853396bd4e61ccb8d5d2c00279
mdimgba <59780947+mdimgba@users.noreply.github.com>
2021-02-17 16:50:40 -0600

    Front end unit testing coverage at 86%

--- src/app/components/update-item/update-item.component.spec.ts
+++ src/app/components/update-item/update-item.component.spec.ts
@@ -8,10 +8,37 @@ import { AngularFireModule } from '@angular/fire';
 
 import { UpdateItemComponent } from './update-item.component';
 import { FormsModule } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 describe('UpdateItemComponent', () => {
+
+class MockService {
+  
+}
+
+// class MockItem {
+//   id: number;
+//   name: string;
+//   type: string;
+//   note: string;
+//   swotAnalysisId: number;
+// }
+let swotItem = [
+  {
+    id: 1,
+    name: "Test",
+    type: "STRENGTH",
+    note: "Test",
+    swotAnalysisId: 1
+}]
+
+
   let component: UpdateItemComponent;
   let fixture: ComponentFixture<UpdateItemComponent>;
+  // let swotServ: SwotService;
+  let modalServ: NgbModal;
+  // let swotItem: SwotItem;
+  let mockClient: {get: jasmine.Spy, post: jasmine.Spy, put: jasmine.Spy, delete: jasmine.Spy}
 
 
   beforeEach(async () => {
@@ -25,8 +52,10 @@ describe('UpdateItemComponent', () => {
         
        ],
         providers: [ HttpClientModule, AngularFireModule, AppRoutingModule, SwotService,
+                    // {provide: SwotItem, useClass: MockItem},
         {
           useClass: class {swotItem = jasmine.createSpy("swotItem")},
+          // useClass: class {swotItem = jasmine.createSpy("swotItem").and.returnValue(of(mockItem))},
         }],
         
       declarations: [ UpdateItemComponent ]
@@ -38,6 +67,8 @@ describe('UpdateItemComponent', () => {
     fixture = TestBed.createComponent(UpdateItemComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    // swotItem = TestBed.inject(SwotItem);
     
   });
 
